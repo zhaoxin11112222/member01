4f30357600e3ffa44725f6d57c851cc343cab221
sunfei2011 <sunpengfei20110701@126.com>
2017-08-17 19:06:40 +0800

    新增member.java文件

---
+++ member.java
@@ -0,0 +1,44 @@
+
+
+public class member {
+	private String name,gender,email,city;
+	private Integer id,age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+}
